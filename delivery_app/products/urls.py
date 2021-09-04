89172f77e0f83a5f72d36fbea7b7ed367e3531e2
Dmitriy Bulygin <128dmitriy128@gmail.com>
2021-09-04 21:10:57 +0500

    Added products list

---
+++ delivery_app/products/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'products'
+urlpatterns = [
+    path('', views.ProductListView.as_view(), name='product_list'),
+    path('<slug:category_slug>/', views.ProductListView.as_view(),
+         name='product_list_by_category'),
+    path('<int:id>/<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
+]
--- delivery_app/products/views.py
+++ delivery_app/products/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+from django.views.generic.base import View
+
+from .models import Category, Product
+
 
 # Create your views here.
+class ProductListView(ListView):
+    def get(self, request, category_slug=None, ordering='AZ', *args, **kwargs):
+        category = None
+        categories = Category.objects.all()
+        products = Product.objects.filter(available=True)
+        if category_slug:
+            category = get_object_or_404(Category, slug=category_slug)
+            products = products.filter(category=category)
+        template_name = 'products/product_list.html'
+        context = {'category': category, 'categories': categories, 'products': products}
+        return render(request, template_name, context)
+
+
+class ProductDetailView(View):
+    def get(self, request, ordering='AZ', *args, **kwargs):
+        product = get_object_or_404(Product, id=self.kwargs['id'], slug=self.kwargs['slug'], available=True)
+        template_name = 'products/product_detail.html'
+        context = {'product': product}
+        return render(request, template_name, context)
