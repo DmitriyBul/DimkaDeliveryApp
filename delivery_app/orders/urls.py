6ceb71dccb1669a3b64405c1aad665a68cf3e3dd
Dmitriy Bulygin <128dmitriy128@gmail.com>
2021-09-30 20:18:15 +0500

    Добавлена генерация и отправка пдф с заказом

--- delivery_app/orders/urls.py
+++ delivery_app/orders/urls.py
@@ -5,4 +5,8 @@ app_name = 'orders'
 urlpatterns = [
     path('create/', views.order_create, name='order_create'),
     path('created/', views.order_created, name='order_created'),
+    path('admin/order/<int:order_id>/', views.admin_order_detail, name='admin_order_detail'),
+    path('admin/order/<int:order_id>/pdf/',
+         views.admin_order_pdf,
+         name='admin_order_pdf'),
 ]
--- delivery_app/payment/views.py
+++ delivery_app/payment/views.py
@@ -3,7 +3,10 @@ from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 from django.conf import settings
 from orders.models import Order
-
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+import weasyprint
+from io import BytesIO
 
 # instantiate Braintree payment gateway
 gateway = braintree.BraintreeGateway(settings.BRAINTREE_CONF)
@@ -31,6 +34,24 @@ def payment_process(request):
             # store the unique transaction id
             order.braintree_id = result.transaction.id
             order.save()
+            # Создание электронного сообщения.
+            subject = 'My Shop - Invoice no. {}'.format(order.id)
+            message = 'Please, find attached the invoice for your recent purchase.'
+            email = EmailMessage(subject,
+                                 message,
+                                 'admin@myshop.com',
+                                 [order.email])
+            # Формирование PDF.
+            html = render_to_string('orders/order/pdf.html', {'order': order})
+            out = BytesIO()
+            stylesheets = [weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')]
+            weasyprint.HTML(string=html).write_pdf(out, stylesheets=stylesheets)
+            # Прикрепляем PDF к электронному сообщению.
+            email.attach('order_{}.pdf'.format(order.id),
+                         out.getvalue(),
+                         'application/pdf')
+            # Отправка сообщения.
+            email.send()
             # launch asynchronous task
             messages.success(request, 'Оплата прошла успешно')
             return redirect('payment:done')
