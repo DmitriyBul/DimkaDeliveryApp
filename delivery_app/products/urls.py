a49e7498ccd8bb9bf5803a6b2f4212b42c282cea
dmitriy <128dmitriy128@gmail.com>
2021-10-25 17:46:41 +0500

    Добавлен раздел с популярными товарами

--- delivery_app/products/urls.py
+++ delivery_app/products/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('search/', views.SearchResultsView.as_view(), name='search_results'),
     path('new/', views.NewProductListView.as_view(),
          name='new_product_list'),
+    path('ranking/', views.ProductsRankingView.as_view(), name='ranking'),
     path('<slug:category_slug>/', views.ProductListView.as_view(),
          name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
--- delivery_app/products/views.py
+++ delivery_app/products/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView
 from django.views.generic.base import View
 from cart.forms import CartAddProductForm
-from .recommender import Recommender
+from .recommender import Recommender, r
 from .forms import CommentForm, SearchForm
 from .models import Category, Product
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -30,26 +30,31 @@ class ProductListView(ListView):
 
 class NewProductListView(ListView):
     def get(self, request, ordering='AZ', *args, **kwargs):
+        categories = Category.objects.all()
         products = Product.objects.filter(available=True).order_by('-created')[:12]
         lst = Paginator(products, 6)
         page_number = request.GET.get('page')
         page_obj = lst.get_page(page_number)
         template_name = 'products/new_product_list.html'
-        context = {'page_obj': page_obj}
+        context = {'page_obj': page_obj, 'categories': categories}
         return render(request, template_name, context)
 
 
 class ProductDetailView(View):
     def get(self, request, ordering='AZ', *args, **kwargs):
         product = get_object_or_404(Product, id=self.kwargs['id'], slug=self.kwargs['slug'], available=True)
-        r = Recommender()
-        recommended_products = r.suggest_products_for([product], 3)
+        z = Recommender()
+        recommended_products = z.suggest_products_for([product], 3)
         cart_product_form = CartAddProductForm()
+        # Увеличиваем количество просмотров товара на 1.
+        total_views = r.incr('product:{}:views'.format(product.id))
+        r.zincrby('product_ranking', product.id, 1)
         comment_form = CommentForm()
         comments = product.comments.filter(active=True)
         template_name = 'products/product_detail.html'
         context = {'product': product, 'cart_product_form': cart_product_form, 'comments': comments,
-                   'comment_form': comment_form, 'recommended_products': recommended_products}
+                   'comment_form': comment_form, 'recommended_products': recommended_products,
+                   'total_views': total_views}
         return render(request, template_name, context)
 
     def post(self, request, ordering='AZ', *args, **kwargs):
@@ -82,3 +87,15 @@ class SearchResultsView(ListView):
             search=SearchVector('name', 'description'),
         ).filter(search=query)
         return product_list
+
+
+class ProductsRankingView(ListView):
+    def get(self, request, ordering='AZ', *args, **kwargs):
+        categories = Category.objects.all()
+        product_ranking = r.zrange('product_ranking', 0, -1, desc=True)[:10]
+        product_ranking_ids = [int(id) for id in product_ranking]
+        most_viewed = list(Product.objects.filter(id__in=product_ranking_ids))
+        most_viewed.sort(key=lambda x: product_ranking_ids.index(x.id))
+        template_name = 'products/products_ranking_results.html'
+        context = {'most_viewed': most_viewed, 'categories': categories}
+        return render(request, template_name, context)
